702e321dc70c0e4da01b125650f3acab3fb1ef46
Felipe Barbosa Carvalho <barbosacarvalhodoamaralfelipe@gmail.com>
2024-10-28 21:10:51 -0300

    camada de xadrez e impressão do tabuleiro

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,5 +1,33 @@
 package boardgame;
 
 public class Position {
-
+	private int row;
+	private int column;
+	
+	
+	public Position(int row, int colum) {
+		this.row = row;
+		this.column = colum;
+	}
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int colum) {
+		this.column = colum;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+	
+	
+	
 }
